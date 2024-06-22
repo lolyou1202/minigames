99c3759c56f9f57d3ceed38402aa919735a292a4
lolyou1202 <sasha555990@gmail.com>
2024-06-22 15:08:26 +0300

    qq

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,16 +1,40 @@
 import './Keyboard.style.scss'
 import { russianAlphabetKeyboard } from '../../constants/alphabet'
+import { BorderedButton } from '../BorderedButton/BorderedButton'
 
 export const Keyboard = () => {
 	return (
 		<div className='keyboard'>
-			{russianAlphabetKeyboard.map((row, index) => (
-				<div key={index} className='keyboard-row'>
-					{row.map(key => (
-						<div key={key} className='keyboard-key'>
+			{russianAlphabetKeyboard.map((row, indexRow) => (
+				<div key={indexRow} className='keyboard-row'>
+					{row.map((key, indexCell) => (
+						<BorderedButton
+							key={indexCell}
+							variant='withoutShadow'
+							background='secondary'
+							className='keyboard-key'
+						>
 							{key}
-						</div>
+						</BorderedButton>
 					))}
+					{indexRow === 1 && (
+						<BorderedButton
+							variant='withoutShadow'
+							background='secondary'
+							className='keyboard-key'
+						>
+							back
+						</BorderedButton>
+					)}
+					{indexRow === 2 && (
+						<BorderedButton
+							variant='withoutShadow'
+							background='secondary'
+							className='keyboard-key enter'
+						>
+							enter
+						</BorderedButton>
+					)}
 				</div>
 			))}
 		</div>
